13800b538406473bc15576cbdfd106afaa2b6a51
Ozge Yavuz <yavuzozge@live.com>
2023-10-25 02:19:06 -0400

    more files around

---
+++ HomeSeerDimmerUnitTests/ConnectionExtensionsTests.cs
@@ -0,0 +1,45 @@
+using FluentAssertions;
+using Moq;
+using NetDaemon.Client;
+using Ozy.HomeSeerDimmers.Apps.Dimmers;
+using static Ozy.HomeSeerDimmers.Apps.Dimmers.ConnectionExtensions;
+
+namespace HomeSeerDimmerUnitTests
+{
+    [TestClass]
+    public class ConnectionExtensionsTests
+    {
+        [TestMethod]
+        public async Task GetDevicesExtendedAsyncCallsConnectionSuccesfully()
+        {
+            // Arrange
+            Mock<IHomeAssistantConnection> mockConnection = new();
+            HassDeviceExtended device = new(
+                "Id",
+                "AreaId",
+                "Name",
+                "NameByUser",
+                "Manufacturer",
+                "Model",
+                new List<IList<string>>
+                {
+                    new List<string>
+                    {
+                        "Identifier1", "Identifier2"
+                    }
+            });
+
+            mockConnection
+                .Setup(m => m.SendCommandAndReturnResponseAsync<BasicHaCommand, IReadOnlyCollection<HassDeviceExtended>>(
+                    It.Is<BasicHaCommand>(c => string.Equals(c.Type, "config/device_registry/list", StringComparison.Ordinal)),
+                    CancellationToken.None))
+                .ReturnsAsync(new[] { device });
+
+            // Act
+            IEnumerable<HassDeviceExtended> result = await mockConnection.Object.GetDevicesExtendedAsync(CancellationToken.None);
+
+            // Assert
+            result.Should().BeEquivalentTo(new[] { device });
+        }
+    }
+}
