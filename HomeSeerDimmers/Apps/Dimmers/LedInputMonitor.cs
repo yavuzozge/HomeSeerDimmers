9a09a8c9871b4f959f210fbe60b53038693f2058
Ozge Yavuz <ozy_dev@outlook.com>
2023-12-04 17:33:09 -0500

    make parsing of monitored entity states more resilient (#33)

--- HomeSeerDimmers/Apps/Dimmers/LedInputMonitor.cs
+++ HomeSeerDimmers/Apps/Dimmers/LedInputMonitor.cs
@@ -17,6 +17,11 @@ namespace Ozy.HomeSeerDimmers.Apps.Dimmers
     public class LedInputMonitor : ILedInputMonitor
     {
         /// <summary>
+        /// logger
+        /// </summary>
+        private readonly ILogger logger;
+
+        /// <summary>
         /// ctor
         /// </summary>
         /// <param name="ha">Home assistant context</param>
@@ -24,10 +29,24 @@ namespace Ozy.HomeSeerDimmers.Apps.Dimmers
         /// <param name="appConfig">App configuration</param>
         public LedInputMonitor(IHaContext ha, ILogger<LedInputMonitor> logger, IAppConfig<Config> appConfig)
         {
-            ArgumentNullException.ThrowIfNull(ha);
-            ArgumentNullException.ThrowIfNull(logger);
+            this.logger = logger;
 
             LedInputTable table = new();
+
+            logger.LogInformation("Setting up LED input monitoring table");
+
+            if (string.IsNullOrWhiteSpace(appConfig.Value.DimmerLedColorEntityNamePattern))
+            {
+                throw new ArgumentException($"{nameof(Config.DimmerLedColorEntityNamePattern)} cannot be emptry or whitespace", nameof(appConfig));
+            }
+            if (string.IsNullOrWhiteSpace(appConfig.Value.DimmerLedBlinkEntityNamePattern))
+            {
+                throw new ArgumentException($"{nameof(Config.DimmerLedBlinkEntityNamePattern)} cannot be emptry or whitespace", nameof(appConfig));
+            }
+            if(string.Equals(appConfig.Value.DimmerLedColorEntityNamePattern, appConfig.Value.DimmerLedBlinkEntityNamePattern, StringComparison.OrdinalIgnoreCase))
+            {
+                throw new ArgumentException($"{nameof(Config.DimmerLedBlinkEntityNamePattern)} should have a different value than {nameof(Config.DimmerLedBlinkEntityNamePattern)}", nameof(appConfig));
+            }
 
             // Create entities and a lookup table that would update the table above
             Dictionary<Entity, (int Index, Action<int, EntityState> UpdateCallback)> entityInputUpdateMap = new();
@@ -39,10 +58,11 @@ namespace Ozy.HomeSeerDimmers.Apps.Dimmers
 
                 entityName = string.Format(appConfig.Value.DimmerLedBlinkEntityNamePattern, i + 1);
                 entityInputUpdateMap.Add(new Entity(ha, entityName), (i, (idx, es) => table = table.CreateWithBlink(idx, GetLedConfigBlink(es))));
-                logger.LogInformation("Monitored for blink: {Name}", entityName);
+                logger.LogInformation("Monitoring for blink: {Name}", entityName);
             };
 
             // Get initial values from entities
+            logger.LogInformation("Getting initial values for the monitoring table");
             foreach (KeyValuePair<Entity, (int Index, Action<int, EntityState> UpdateCallback)> kvp in entityInputUpdateMap)
             {
                 EntityState? state = kvp.Key.EntityState;
@@ -51,6 +71,8 @@ namespace Ozy.HomeSeerDimmers.Apps.Dimmers
                     kvp.Value.UpdateCallback(kvp.Value.Index, state);
                 }
             }
+
+            logger.LogInformation("Setting up the monitoring table to receive state changes");
 
             // Always return the latest value on subscribe
             IObservable<LedInputTable> latest = Observable.Return(table);
@@ -85,9 +107,22 @@ namespace Ozy.HomeSeerDimmers.Apps.Dimmers
         /// </summary>
         /// <param name="state">Entity state</param>
         /// <returns>Result</returns>
-        private static LedStatusColor GetLedConfigStatusColor(EntityState state)
+        private LedStatusColor GetLedConfigStatusColor(EntityState state)
         {
-            return Enum.Parse<LedStatusColor>(state.State ?? "off", ignoreCase: true);
+            // Special case entity being unavailable as color 'off'
+            if (string.Equals(state.State, "unavailable",StringComparison.OrdinalIgnoreCase))
+            {
+                return LedStatusColor.Off;
+            }
+
+            if (Enum.TryParse(state.State ?? "off", ignoreCase: true, out LedStatusColor color))
+            {
+                return color;
+            }
+
+            this.logger.LogWarning("Failed to parse LED status color entity {EntityId} with state {State}: ", state.EntityId, state.State);
+
+            return LedStatusColor.Off;
         }
 
         /// <summary>
@@ -97,7 +132,7 @@ namespace Ozy.HomeSeerDimmers.Apps.Dimmers
         /// <returns>Result</returns>
         private static LedBlink GetLedConfigBlink(EntityState state)
         {
-            return string.Equals(state.State, "off", StringComparison.InvariantCultureIgnoreCase) ? LedBlink.Off : LedBlink.On;
+            return string.Equals(state.State, "on", StringComparison.InvariantCultureIgnoreCase) ? LedBlink.On : LedBlink.Off;
         }
     };
 }
