ac4064168528296637d04ac294319b888806da2e
Ozge Yavuz <yavuzozge@live.com>
2023-12-04 17:54:38 -0500

    add uts

---
+++ HomeSeerDimmerUnitTests/LedInputTableExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Ozy.HomeSeerDimmers.Apps.Dimmers;
+using Ozy.HomeSeerDimmers.Apps.Dimmers.HomeSeerDevice;
+
+namespace HomeSeerDimmerUnitTests
+{
+    /// <summary>
+    /// Unit tests for <see cref="LedInputTableExtensions"/>
+    /// </summary>
+    [TestClass]
+    public class LedInputTableExtensionsTests
+    {
+        [TestMethod]
+        public void CreateWithColorCreatesANewTableAndUpdatesTheValue()
+        {
+            // Arrange
+            LedInputTable table = new();
+
+            // Act
+            LedInputTable table2 = table.CreateWithColor(1, LedStatusColor.Blue);
+
+            // Assery
+            table.Should().NotBeSameAs(table2);
+            table.Colors[1].Should().Be(LedStatusColor.Off);
+            table2.Colors[1].Should().Be(LedStatusColor.Blue);
+        }
+
+        [TestMethod]
+        public void CreateWithBlinkCreatesANewTableAndUpdatesTheValue()
+        {
+            // Arrange
+            LedInputTable table = new();
+
+            // Act
+            LedInputTable table2 = table.CreateWithBlink(1, LedBlink.On);
+
+            // Assery
+            table.Should().NotBeSameAs(table2);
+            table.Blinks[1].Should().Be(LedBlink.Off);
+            table2.Blinks[1].Should().Be(LedBlink.On);
+        }
+    }
+}
